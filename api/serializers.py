49bfbaace0b97d078e1aaab37d20dbe51aaf7068
Adiazx14 <adrianvaldes1439@gmail.com>
2021-08-25 23:31:34 -0400

    added serializers and first view

---
+++ api/serializers.py
@@ -0,0 +1,65 @@
+from django.db.models import fields
+from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+from .models import Article, ArticleSection, Author, Image, Paragraph, Quote
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = "__all__"
+
+class ImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = "__all__"
+
+class ParagraphsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paragraph
+        fields = "__all__"
+
+class QuotesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quote
+        fields = "__all__"
+
+class ArticleSectionSerializer(serializers.ModelSerializer):
+    paragraphs = serializers.SerializerMethodField(read_only=True)
+    images = serializers.SerializerMethodField(read_only=True)
+    quotes = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = ArticleSection
+        fields = "__all__"
+
+    def get_paragraphs(self, obj):
+        paragraphs = obj.paragraph_set.all()
+        serializer = ParagraphsSerializer(paragraphs, many=True)
+        return serializer.data
+
+    def get_images(self, obj):
+        images = obj.image_set.all()
+        serializer = ImagesSerializer(images, many=True)
+        return serializer.data
+
+    def get_quotes(self, obj):
+        quotes = obj.quote_set.all()
+        serializer = QuotesSerializer(quotes, many=True)
+        return serializer.data
+ 
+
+class ArticleSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField(read_only=True)
+    sections = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Article
+        fields = "__all__"
+
+    def get_author(self, obj):
+        return str(obj.author)
+
+    def get_sections(self, obj):
+        sections = obj.articlesection_set.all()
+        serializer = ArticleSectionSerializer(sections, many=True)
+        return serializer.data
--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
+from api.serializers import ArticleSerializer
+from api.models import Article
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView, status
 
-# Create your views here.
+class ArticleList(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
