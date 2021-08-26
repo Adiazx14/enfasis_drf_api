d0c61d4215fd85fdaa5b41a25e00227157256426
Adiazx14 <adrianvaldes1439@gmail.com>
2021-08-25 22:55:04 -0400

    added models to admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.db import models
+from .models import Article, ArticleSection, Author, Image, Paragraph, Quote
+
+class ArticleSectionInline(admin.TabularInline):
+    model = ArticleSection
+
+class ArticleAdmin(admin.ModelAdmin):
+    model= Article
+    inlines=[ArticleSectionInline]
+
+class ParagraphsInline(admin.TabularInline):
+    model=Paragraph
+
+class ImagesInline(admin.TabularInline):
+    model=Image
+
+class QuotesInline(admin.TabularInline):
+    model=Quote
+
+class ArticleSectionAdmin(admin.ModelAdmin):
+    model=ArticleSection
+    inlines=[ParagraphsInline, ImagesInline, QuotesInline]
+
 
 # Register your models here.
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleSection,ArticleSectionAdmin)
+admin.site.register(Author)
--- api/models.py
+++ api/models.py
@@ -6,16 +6,25 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Article(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     date_published = models.DateTimeField(auto_now_add=True)
     subtitle = models.CharField(max_length=200, blank=True)
 
+    def __str__(self):
+        return self.title
+
 class ArticleSection(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, blank=True)
     order = models.IntegerField()
+
+    def __str__(self):
+        return  str(self.article) + " (" + str(self.order) + ")"
 
 class Image(models.Model):
     section = models.ForeignKey(ArticleSection, on_delete=models.CASCADE)
