9f8242e540766e332d41e68d25f99b8caffc49ff
Adiazx14 <adrianvaldes1439@gmail.com>
2021-09-24 00:05:16 -0400

    Remove field article from image

--- api/models.py
+++ api/models.py
@@ -57,7 +57,6 @@ class Revista(models.Model):
 
 class Image(models.Model):
     section = models.ForeignKey(ArticleSection, on_delete=models.CASCADE, null=True, blank=True)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
     revista = models.ForeignKey(Revista, on_delete=models.CASCADE, null=True, blank=True)
     caption = models.CharField(max_length=200, blank=True)
     image = models.ImageField()
